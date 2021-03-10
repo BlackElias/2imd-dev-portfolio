1dd8211417a7965e7464e21409c58ec78ddffec8
BlackElias <valienne.elias@outlook.com>
2021-03-10 13:49:29 +0100

    added api url connection

--- lab4/ads.js
+++ lab4/ads.js
@@ -11,10 +11,16 @@ class App {
   let lng = position.coords.longitude;
   console.log(lat);
   console.log(lng);
-
+this.getWeather(lat, lng);
 
 });
  }
+getWeather(lat, lng){
+  let url = 'http://api.weatherstack.com/current?access_key=c32edf305c839b42e5dc85d81d0310ff&query=Zemst&units=m';
+  fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data));
+  };
 
 
 
