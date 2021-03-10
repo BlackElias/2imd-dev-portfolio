e59b1af8b7e292482af5cbd46fad12e3a331d2fe
BlackElias <valienne.elias@outlook.com>
2021-03-10 14:56:53 +0100

    added response and temperature working

--- lab4/ads.js
+++ lab4/ads.js
@@ -7,31 +7,34 @@ class App {
 
  geo(){
  navigator.geolocation.getCurrentPosition((position) => {
-  let lat = position.coords.latitude;
-  let lng = position.coords.longitude;
-  console.log(lat);
-  console.log(lng);
-this.getWeather(lat, lng);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(latitude, longitude)
+  this.getWeather( latitude, longitude);
+
 
 });
  }
-getWeather(lat, lng){
-  const params = {
-    access_key: 'c32edf305c839b42e5dc85d81d0310ff',
-    query: 'Zemst'
-  }
-  let url = `http://api.weatherstack.com/current ${params}`;
+getWeather(latitude, longitude){
+  console.log(latitude, longitude)
+  let url = `http://api.weatherstack.com/current?access_key=c32edf305c839b42e5dc85d81d0310ff&query=${latitude},${longitude} `;//${latitude},${longitude}
+  console.log(url);
   fetch(url)
-  .then(response => {
-    const apiResponse = response.data;
-    let temp = apiResponse.current.temperature;
+  .then((response) => {
+    return response.json();
+    
+  }).then((json) =>{
+    console.log(json)
+    
+    let temp = json.current.temperature;
     console.log(`Current temperature in Zemst is ${temp}℃`);
-  }).catch(error => {
-    console.log(error);
+  });
+   
+
   
 
   //this.ad(temperature, text);
-  });
+  
 
 //ad(temp, text );{
 
