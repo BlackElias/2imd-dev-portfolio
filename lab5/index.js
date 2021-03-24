2aeb89acaa654e355524c81a3d89a45cab7f142e
BlackElias <valienne.elias@outlook.com>
2021-03-24 11:25:50 +0100

    added redirections

--- lab5/index.js
+++ lab5/index.js
@@ -4,3 +4,13 @@ const prt = 3000;
 
 const routeMessages = require('./routers/api/v1/messages');
 const pug = require('pug');
+
+app.set('view engine', 'pug'); 
+app.get("/", (req, res) =>{ 
+    res.render('index');
+});
+app.use("/api/v1/messages", routeMessages);
+
+app.listen(prt, () => {
+  console.log(`Example app listening at http://localhost:${prt}`)
+})
