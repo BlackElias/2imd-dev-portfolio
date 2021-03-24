3c474eeaf9d150cf87f368f775479d2ee8473e77
BlackElias <valienne.elias@outlook.com>
2021-03-24 10:44:00 +0100

    added pug

--- lab5/index.js
+++ lab5/index.js
@@ -2,3 +2,5 @@ const express = require("express");
 const app = express();
 const prt = 3000;
 const router = require("./routers/messages");
+const routeMessages = require('./routers/messages');
+const pug = require('pug');
